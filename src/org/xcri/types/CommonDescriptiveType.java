ff726ddd3bb9c1f6e2e2d001899caf9832397e54
scottbw <scott.bradley.wilson@gmail.com>
2012-06-21 12:03:32 +0100

    started adding common descriptive types

--- src/org/xcri/types/CommonDescriptiveType.java
+++ src/org/xcri/types/CommonDescriptiveType.java
@@ -19,17 +19,81 @@
  */
 package org.xcri.types;
 
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jdom.Element;
+import org.xcri.Namespaces;
+import org.xcri.common.descriptive.*;
 import org.xcri.exceptions.InvalidElementException;
+import org.xcri.util.lax.Lax;
 
 public class CommonDescriptiveType extends CommonType {
+	
+	private Log log = LogFactory.getLog(CommonDescriptiveType.class);
+	
+	private Abstract[] abstracts;
+	private ApplicationProcedure[] applicationProcedures;
+	private Assessment[] assessments;
+	private LearningOutcome[] learningOutcomes;
+	private Objective[] objectives;
+	private Prerequisite[] prerequisites;
+	private Regulations[] regulations;
 
 	/* (non-Javadoc)
 	 * @see org.xcri.types.CommonType#toXml()
 	 */
 	@Override
 	public Element toXml() {
-		return super.toXml();
+		Element element = super.toXml();
+		
+		//
+		// Descriptions...
+		//
+		ArrayList<Abstract> abstracts = new ArrayList<Abstract>();
+		for (Object obj: Lax.getChildrenQuietly(element, "abstract", Namespaces.XCRI_NAMESPACE_NS, log)){
+			Abstract anabstract = new Abstract();
+			try {
+				anabstract.fromXml((Element)obj);
+				abstracts.add(anabstract);
+			} catch (InvalidElementException e) {
+				log.warn(this.getName()+" : skipping invalid abstract element: "+e.getMessage());
+			}
+		}
+		this.setAbstracts(abstracts.toArray(new Abstract[abstracts.size()]));
+		
+		ArrayList<ApplicationProcedure> applicationProcedures = new ArrayList<ApplicationProcedure>();
+		for (Object obj: Lax.getChildrenQuietly(element, "applicationProcedure", Namespaces.XCRI_NAMESPACE_NS, log)){
+			ApplicationProcedure applicationProcedure = new ApplicationProcedure();
+			try {
+				applicationProcedure.fromXml((Element)obj);
+				applicationProcedures.add(applicationProcedure);
+			} catch (InvalidElementException e) {
+				log.warn(this.getName()+" : skipping invalid applicationProcedure element: "+e.getMessage());
+			}
+		}
+		this.setApplicationProcedures(applicationProcedures.toArray(new ApplicationProcedure[applicationProcedures.size()]));
+		
+		
+		ArrayList<Assessment> assessments = new ArrayList<Assessment>();
+		for (Object obj: Lax.getChildrenQuietly(element, "assessment", Namespaces.MLO_NAMESPACE_NS, log)){
+			Assessment assessment = new Assessment();
+			try {
+				assessment.fromXml((Element)obj);
+				assessments.add(assessment);
+			} catch (InvalidElementException e) {
+				log.warn(this.getName()+" : skipping invalid assessment element: "+e.getMessage());
+			}
+		}
+		this.setAssessments(assessments.toArray(new Assessment[assessments.size()]));
+		
+		// TODO learningoutcome
+		// TODO objective
+		// TODO prerequisite
+		// TODO regulations
+		
+		return element;
 	}
 
 	/* (non-Javadoc)
@@ -38,6 +102,118 @@ public class CommonDescriptiveType extends CommonType {
 	@Override
 	public void fromXml(Element element) throws InvalidElementException {
 		super.fromXml(element);
+		
+		//
+		// Descriptions...
+		//
+		
+		if (this.getAbstracts() != null) for (Abstract a: this.getAbstracts()) element.addContent(a.toXml());
+		if (this.getApplicationProcedures() != null) for (ApplicationProcedure a: this.getApplicationProcedures()) element.addContent(a.toXml());
+		if (this.getAssessments() != null) for (Assessment a: this.getAssessments()) element.addContent(a.toXml());
+		if (this.getLearningOutcomes() != null) for (LearningOutcome a: this.getLearningOutcomes()) element.addContent(a.toXml());
+		if (this.getObjectives() != null) for (Objective a: this.getObjectives()) element.addContent(a.toXml());
+		if (this.getPrerequisites() != null) for (Prerequisite a: this.getPrerequisites()) element.addContent(a.toXml());
+		if (this.getRegulations() != null) for (Regulations a: this.getRegulations()) element.addContent(a.toXml());
+
+	}
+
+	/**
+	 * @return the abstracts
+	 */
+	public Abstract[] getAbstracts() {
+		return abstracts;
+	}
+
+	/**
+	 * @param abstracts the abstracts to set
+	 */
+	public void setAbstracts(Abstract[] abstracts) {
+		this.abstracts = abstracts;
+	}
+
+	/**
+	 * @return the applicationProcedures
+	 */
+	public ApplicationProcedure[] getApplicationProcedures() {
+		return applicationProcedures;
+	}
+
+	/**
+	 * @param applicationProcedures the applicationProcedures to set
+	 */
+	public void setApplicationProcedures(
+			ApplicationProcedure[] applicationProcedures) {
+		this.applicationProcedures = applicationProcedures;
+	}
+
+	/**
+	 * @return the assessments
+	 */
+	public Assessment[] getAssessments() {
+		return assessments;
+	}
+
+	/**
+	 * @param assessments the assessments to set
+	 */
+	public void setAssessments(Assessment[] assessments) {
+		this.assessments = assessments;
+	}
+
+	/**
+	 * @return the learningOutcomes
+	 */
+	public LearningOutcome[] getLearningOutcomes() {
+		return learningOutcomes;
+	}
+
+	/**
+	 * @param learningOutcomes the learningOutcomes to set
+	 */
+	public void setLearningOutcomes(LearningOutcome[] learningOutcomes) {
+		this.learningOutcomes = learningOutcomes;
+	}
+
+	/**
+	 * @return the objectives
+	 */
+	public Objective[] getObjectives() {
+		return objectives;
+	}
+
+	/**
+	 * @param objectives the objectives to set
+	 */
+	public void setObjectives(Objective[] objectives) {
+		this.objectives = objectives;
+	}
+
+	/**
+	 * @return the prerequisites
+	 */
+	public Prerequisite[] getPrerequisites() {
+		return prerequisites;
+	}
+
+	/**
+	 * @param prerequisites the prerequisites to set
+	 */
+	public void setPrerequisites(Prerequisite[] prerequisites) {
+		this.prerequisites = prerequisites;
+	}
+
+	/**
+	 * @return the regulations
+	 */
+	public Regulations[] getRegulations() {
+		return regulations;
+	}
+
+	/**
+	 * @param regulations the regulations to set
+	 */
+	public void setRegulations(Regulations[] regulations) {
+		this.regulations = regulations;
 	}
 	
 	
