6d543a6ba6e0d0523bf2764c87aac0a99477465a
scottbw <scott.bradley.wilson@gmail.com>
2012-06-21 08:29:19 +0100

    Added CDATA handling and associated test data and test cases

---
+++ src-test/org/xcri/util/HtmlTest.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2011-2012 University of Bolton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.xcri.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+import org.junit.Test;
+import org.xcri.common.Description;
+import org.xcri.core.Catalog;
+import org.xcri.exceptions.InvalidElementException;
+
+public class HtmlTest {
+	
+	@Test
+	public void parseCdata() throws IOException, JDOMException, InvalidElementException{
+		
+		Catalog catalog = new Catalog();
+		SAXBuilder builder = new SAXBuilder();
+		
+		Document document = builder.build(new File("src-test/cdata_test.xml"));
+		
+		catalog.fromXml(document);
+		
+		Description description = catalog.getProviders()[0].getCourses()[0].getDescriptions()[0];
+		
+		String output = new XMLOutputter().outputString(description.toXml());
+		
+		assertEquals("<dc:description xmlns:dc=\"http://purl.org/dc/elements/1.1/\"><xhtml:div xmlns:xhtml=\"http://www.w3.org/1999/xhtml\"><xhtml:p>This is a computing course, its great!</xhtml:p></xhtml:div></dc:description>", output);
+	}
+	
+	@Test
+	public void parsePlain() throws IOException, JDOMException, InvalidElementException{
+		
+		Catalog catalog = new Catalog();
+		SAXBuilder builder = new SAXBuilder();
+		
+		Document document = builder.build(new File("src-test/cdata_plain_test.xml"));
+		
+		catalog.fromXml(document);
+		
+		Description description = catalog.getProviders()[0].getCourses()[0].getDescriptions()[0];
+		
+		String output = new XMLOutputter().outputString(description.toXml());
+		
+		assertEquals("<dc:description xmlns:dc=\"http://purl.org/dc/elements/1.1/\"><xhtml:div xmlns:xhtml=\"http://www.w3.org/1999/xhtml\">This is a computing course, its great!</xhtml:div></dc:description>", output);
+	}
+	
+	@Test
+	public void parseXhtml() throws IOException, JDOMException, InvalidElementException{
+		
+		Catalog catalog = new Catalog();
+		SAXBuilder builder = new SAXBuilder();
+		
+		Document document = builder.build(new File("src-test/xhtml_test.xml"));
+		
+		catalog.fromXml(document);
+		
+		Description description = catalog.getProviders()[0].getCourses()[0].getDescriptions()[0];
+		
+		String output = new XMLOutputter().outputString(description.toXml());
+		
+		assertEquals("<dc:description xmlns:dc=\"http://purl.org/dc/elements/1.1/\"><div xmlns=\"http://www.w3.org/1999/xhtml\"><p>This is a computing course, its great!</p></div></dc:description>", output);
+	}
+
+}
--- src/org/xcri/types/DescriptiveTextType.java
+++ src/org/xcri/types/DescriptiveTextType.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011 University of Bolton
+ * Copyright (c) 2011-2012 University of Bolton
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
  * software and associated documentation files (the "Software"), to deal in the Software 
@@ -20,13 +20,23 @@
 package org.xcri.types;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.PrettyXmlSerializer;
+import org.htmlcleaner.TagNode;
+import org.jdom.CDATA;
+import org.jdom.Content;
+import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 import org.jdom.output.XMLOutputter;
 import org.xcri.Namespaces;
 import org.xcri.ParserConfiguration;
@@ -61,47 +71,28 @@ public class DescriptiveTextType extends XcriElement{
 	@Override
 	public void fromXml(Element element) throws InvalidElementException {
 		super.fromXml(element);
-
-
+		
 		//
 		// Add XHTML content if present
 		//
 		if (element.getChild("div", Namespaces.XHTML_NAMESPACE_NS)!= null){
-			XMLOutputter out = new XMLOutputter();
-			StringWriter writer = new StringWriter();
-			xhtml = (Element) element.getChild("div", Namespaces.XHTML_NAMESPACE_NS).detach();
-			try {
-				//
-				// Check for potentially dangerous elements
-				//
-				Iterator i = xhtml.getDescendants(new ContentSecurityFilter());
-				ArrayList<Element> toRemove = new ArrayList<Element>();
-				while (i.hasNext()){
-					Element dangerousElement = (Element) i.next();
-					toRemove.add(dangerousElement);
-					log.warn("description : content contains potentially dangerous XHTML :"+dangerousElement.getName());
+			processXhtml((Element) element.getChild("div", Namespaces.XHTML_NAMESPACE_NS).detach());
+		}
+		
+		//
+		// How about some CDATA with HTML in it?
+		//
+		for (Object child: element.getContent()){
+			if (child instanceof CDATA){
+				if (ParserConfiguration.getInstance().fixCDATA()){
+				log.warn("description: uses CDATA instead of XHTML. Attempting to convert inline HTML into XHTML.");
+				processCDATA((CDATA)child);
+				} else {
+					throw new InvalidElementException("description: contains CDATA. To allow CDATA to be converted to XHTML, run with fixCDATA=true");
 				}
-				
-				//
-				// If configured to do so, remove elements
-				//
-				if (ParserConfiguration.getInstance().sanitizeXHTML()){
-					for (Element dangerousElement: toRemove){
-						log.warn("description : removing XHTML element :"+dangerousElement.getName());
-						dangerousElement.getParentElement().removeContent(dangerousElement);					
-					}
-				}
-				
-				out.output(xhtml, writer);
-				this.setValue(writer.getBuffer().toString());
-				isXhtml = true;
-
-
-			} catch (IOException e) {
-				throw new InvalidElementException("Error reading content of description element");
 			}
 		}
-
+		
 		//
 		// Add HREF
 		//
@@ -110,10 +101,101 @@ public class DescriptiveTextType extends XcriElement{
 		//
 		// Cannot have both linked and inline content
 		//
-		if (this.getValue()!=null && this.getValue().length()>0){
+		if ((this.getValue()!=null && this.getValue().length()>0) || this.xhtml != null){
 			if(this.getHref()!= null && this.getHref().length()>0){
 				throw new InvalidElementException("Description contains both text content and href attribute; only one or the other may be used");
 			}
+		}
+	}
+	
+	/**
+	 * Attempt to process a CDATA section as HTML, and convert to XHTML
+	 * @param cdata
+	 * @throws InvalidElementException
+	 */
+	private void processCDATA(CDATA cdata) throws InvalidElementException{
+		
+		//
+		// Use HTMLCleaner to clean the content
+		//
+		HtmlCleaner cleaner = new HtmlCleaner();
+		TagNode node = cleaner.clean(cdata.getValue());
+		
+		//
+		// Output the HTML as XML, and read in using JDOM
+		//
+		PrettyXmlSerializer ser = new PrettyXmlSerializer(cleaner.getProperties());
+		try {
+			String out = ser.getAsString(node);
+			Element element = new Element("div", Namespaces.XHTML_NAMESPACE_NS);
+			SAXBuilder builder = new SAXBuilder();
+			Document document = builder.build(new StringReader(out));
+			
+			//
+			// Add all children of <body> to a new <div> element
+			//
+			@SuppressWarnings("unchecked")
+			List<Content> li = document.getRootElement().getChild("body").getChildren();
+			for(int i=0;i<li.size();i++){
+				Element next = (Element)li.get(i);
+				next.detach();
+				next.setNamespace(Namespaces.XHTML_NAMESPACE_NS);
+				element.addContent(next);
+			}
+
+			//
+			// Finally, add any raw text
+			//
+			element.addContent(document.getRootElement().getChild("body").getTextTrim());	
+			
+			//
+			// Post-process the XHTML
+			//
+			processXhtml(element);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	private void processXhtml(Element element) throws InvalidElementException{
+		XMLOutputter out = new XMLOutputter();
+		StringWriter writer = new StringWriter();
+		xhtml = element;
+		try {
+			//
+			// Check for potentially dangerous elements
+			//
+			@SuppressWarnings("rawtypes")
+			Iterator i = xhtml.getDescendants(new ContentSecurityFilter());
+			ArrayList<Element> toRemove = new ArrayList<Element>();
+			while (i.hasNext()){
+				Element dangerousElement = (Element) i.next();
+				toRemove.add(dangerousElement);
+				log.warn("description : content contains potentially dangerous XHTML :"+dangerousElement.getName());
+			}
+			
+			//
+			// If configured to do so, remove elements
+			//
+			if (ParserConfiguration.getInstance().sanitizeXHTML()){
+				for (Element dangerousElement: toRemove){
+					log.warn("description : removing XHTML element :"+dangerousElement.getName());
+					dangerousElement.getParentElement().removeContent(dangerousElement);					
+				}
+			}
+			
+			out.output(xhtml, writer);
+			this.setValue(writer.getBuffer().toString());
+			isXhtml = true;
+
+		} catch (IOException e) {
+			throw new InvalidElementException("Error reading content of description element");
 		}
 	}
 
@@ -125,6 +207,13 @@ public class DescriptiveTextType extends XcriElement{
 		Element element = super.toXml();
 		if (this.getHref() != null) element.setAttribute("href", this.getHref());
 		if (isXhtml){
+			//
+			// Remove plain content
+			//
+			element.removeContent();
+			//
+			// Add XHTML
+			//
 			element.addContent(xhtml);
 		}
 		return element;
